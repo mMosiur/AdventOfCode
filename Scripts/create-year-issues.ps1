870600bed239e2261adc9cb371bc59b879caba17
Mateusz Piotr Moruś <mpmorus@gmail.com>
2024-04-12 13:30:11 +0200

    Change script to be more script-like

--- Scripts/create-year-issues.ps1
+++ Scripts/create-year-issues.ps1
@@ -1,29 +1,50 @@
-$aocYear = 2023
-$firstDayIssue = 1
-$lastDayIssue = 25
+[CmdletBinding()]
+param (
+    [Parameter()]
+    [int]$Year = (Get-Date).Year,
+    [Parameter()]
+    [int]$FirstDay = 1,
+    [Parameter()]
+    [int]$LastDay = 25,
+    [Parameter()]
+    [string]$Milestone = "Advent of Code $Year",
+    [Parameter()]
+    [string]$Assignee = "@me",
+    [Parameter()]
+    [string]$ProjectName = "Advent of Code solving"
+)
 
-$milestone = "Advent of Code $aocYear"
-$assignee = "@me"
-$projectName = "Advent of Code solving"
-$repository = "mMosiur/AdventOfCode$aocYear"
+$repository = "mMosiur/AdventOfCode$Year"
 
-# Print the script config
-Write-Host "Creating issues for Advent of Code $aocYear"
-Write-Host "Repository: $repository"
-Write-Host "In scope: days from $firstDayIssue to $lastDayIssue"
-Write-Host "Assigning to: $assignee"
-Write-Host "Adding to project: $projectName"
-Write-Host "Adding to milestone: $milestone"
-
-# Ask user for confirmation, default to yes
-$confirmation = Read-Host "Do you want to continue? (Y/n)"
-if ($confirmation -ne "Y" -and $confirmation -ne "") {
-    Write-Host "Aborted."
-    exit
+# Check if GitHub CLI is installed
+if (-not (gh --version)) {
+    Write-Error "GitHub CLI is not installed."
+    exit 1
 }
 
+# Check if the repository exists
+$repositoryExists = gh repo view $repository --json "name" | ConvertFrom-Json
+if (-not $repositoryExists) {
+    Write-Error "Repository '$repository' does not exist."
+    exit 1
+}
 
-for ($i = $firstDayIssue; $i -le $lastDayIssue; $i++) {
+# Print the script config
+Write-Host "Creating issues for Advent of Code $Year"
+Write-Host "Repository: $repository"
+Write-Host "In scope: days from $FirstDay to $LastDay"
+Write-Host "Assigning to: $Assignee"
+Write-Host "Adding to project: $ProjectName"
+Write-Host "Adding to milestone: $Milestone"
+
+# Ask user for confirmation, default to no
+$confirmation = Read-Host "Do you want to continue? (y/N)"
+if ($confirmation -notmatch "^(y|ye|yes)$") {
+    Write-Host "Aborted."
+    exit 1
+}
+
+for ($i = $FirstDay; $i -le $LastDay; $i++) {
     $issueTitle = "Solve day $i"
 
     # Check if issue with the same title already exists
@@ -34,6 +55,6 @@ for ($i = $firstDayIssue; $i -le $lastDayIssue; $i++) {
     }
 
     Write-Host "Creating issue: $issueTitle"
-    $issueBody = "Solve both parts of [day $i of Advent of Code $aocYear](https://adventofcode.com/$aocYear/day/$i)"
-    gh issue create --title "$issueTitle" --body "$issueBody" --project "$projectName" --milestone "$milestone" --assignee "$assignee" --repo "$repository"
+    $issueBody = "Solve both parts of [day $i of Advent of Code $Year](https://adventofcode.com/$Year/day/$i)"
+    gh issue create --title "$issueTitle" --body "$issueBody" --project "$ProjectName" --milestone "$Milestone" --assignee "$Assignee" --repo "$repository"
 }
